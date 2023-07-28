d8fb21721c09dbf3eb26d233c9214adc4db3df41
marciiosouza <marciiosouza@outlook.com>
2023-07-27 21:24:05 -0300

    Preparando o ambiente e criando components em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite o seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+     
     </div>
   );
 }
