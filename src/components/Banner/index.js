7b6d182aa74be71ba747705e8a094a6e57c2aae8
marciiosouza <marciiosouza@outlook.com>
2023-07-31 22:30:58 -0300

    Ajuste no index.js do componente banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,11 +3,9 @@ import "./Banner.css";
 export const Banner = () => {
     
   return (
-    <Fragment>
       <header className="banner">
         <img src="/imagens/banner.png" alt="O Banner principal do organo" />
       </header>
-    </Fragment>
   )
 }
 
